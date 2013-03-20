41f0520540c342fd1254f7c0ca05f1f3c45ad63a
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-03-20 09:19:59 +0800

    版本模板

--- include/version-template.h
+++ include/version-template.h
@@ -1,8 +1,11 @@
-/** @file version-template.h控制版本,被 pubilsh.sh所使用.
- version-template.h 是模板文件.
- version.h 是由模板文件生成的版本文件.
- 使用 makeversion.sh 脚本将根据git版本由模板文件生成version.h文件
- 请在需要时直接修改模板文件,而不是修改生成的<version.h>文件!
+/** 
+ * version.h
+ * @file version-template.h
+ * 版本号文件,发布时用于生成版本号.
+ * version-template.h 是模板文件.
+ * version.h 是由模板文件生成的版本文件.
+ * 运行 makeversion.sh 脚本将根据git版本由模板文件生成version.h文件
+ * 请在需要时直接修改模板文件,而不是修改生成的 version.h 文件!
  */
 #ifndef _VERSION_H_
 #define _VERSION_H_
