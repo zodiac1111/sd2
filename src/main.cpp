422bd95035d98578844b5919099558e4b6888614
zodiac1111 <zodiac8765+github_admin@gmail.com>
2013-03-20 13:23:07 +0800

    工程设置

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,27 @@
 #include <stdio.h>
 #include "main.h"
 #include "info.h"
-#include "define.h"
+//#include "define.h"
 #include "sys_utl.h"
-#include "loopbuf.h"
-#include "rtclocklib.h"
+//#include "loopbuf.h"
+//#include "rtclocklib.h"
 #include "protocol.h"
 
 extern "C" CProtocol * CreateCProto_sd2()
 {
-	PRINT_VERSION
-	PRINT_BUILD_TIME
 	return new Csd2;
 }
+
 Csd2::Csd2()
 {
+	PRINT_VERSION
+	PRINT_BUILD_TIME
+}
+void Csd2::SendProc(void)
+{
 
+}
+int Csd2::ReciProc(void)
+{
+	return 0;
 }
